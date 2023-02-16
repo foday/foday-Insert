4b3f48cef4e98edfda553bbc8167e6ed780d4f01
Sim <foday_s@yahoo.com>
2023-02-16 01:06:45 +0000

    complete INSERT assignment

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,3 @@
-write your sql statement in a single line in the problem1.sql file.
+ INSERT INTO song (title, artist) VALUES ('let it be', 'Beatles');
+
+-- write your sql statement in a single line in the problem1.sql file.
